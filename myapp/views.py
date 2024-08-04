b61f76940a526f68911d96bff2b11ba884fa5746
Shreya Jogi <shreya75jogi@gmail.com>
2024-08-04 13:25:34 +0530

    Initial commit

---
+++ myapp/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render,HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request,"index.html")
+
+def index9(request):
+    return render(request,"index9.html")
+
+def about(request):
+    return render(request,"about.html")
+
+def login(request):
+    return render(request,"login.html")
+
+def register(request):
+    return render(request,"register.html")
+
+def errorpage(request):
+    return render(request,"404.html")    
+
+def contact(request):
+    return render(request,"contact.html")
+    
+def help(request):
+    return render(request,"help.html")  
+
+def projects(request):
+    return render(request,"projects.html") 
