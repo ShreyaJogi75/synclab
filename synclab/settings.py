fd1986f2ecf58bb7343b5d38212cce0edbd57352
Dharmik Vaghela <160165247+Dharmik-Vaghela@users.noreply.github.com>
2024-08-12 11:16:50 +0530

    Login

--- synclab/settings.py
+++ synclab/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'synclab',
         'USER': 'root',
-        'PASSWORD': '1209',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
     }
